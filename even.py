85743b92a6f13dbe8fe72e4841d55821ea7d749c
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-01 17:18:14 +0200

    Add files via upload

---
+++ even.py
@@ -0,0 +1,33 @@
+# asking the user to enter a number using while loops 
+
+# ask user to enter number using the int(function),input(function)
+number = int(input("Enter any number:"))
+# created variables to complete the while loop function
+even_numbers = 0 
+
+# used the while loop to print all the even numbers and increment it by 2 
+while even_numbers <= number:
+      
+      print(even_numbers)
+      
+      even_numbers = even_numbers + 2
+      
+      
+
+
+      
+      
+
+
+
+
+
+     
+
+
+    
+
+
+
+
+  
